a9c774cbe86d8049e68b38e2ada380977432ffb2
Felix Krause <krausefx@gmail.com>
2017-08-12 12:51:44 -0700

    Got it compiling without Xcode crashing after a while, is this the future?

---
+++ Overkill/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Overkill
+//
+//  Created by Felix Krause on 8/12/17.
+//  Copyright © 2017 Felix Krause. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var statusMenu: NSMenu!
+
+    @IBAction func didClickExit(_ sender: Any) {
+    }
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
