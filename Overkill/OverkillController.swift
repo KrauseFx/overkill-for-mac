9906ec619abc1f43999dbba7935b836c6c7a9a9a
Felix Krause <krausefx@gmail.com>
2017-08-12 13:33:06 -0700

    Add auto-killing of iTunes and Photos app

--- Overkill/OverkillController.swift
+++ Overkill/OverkillController.swift
@@ -12,12 +12,15 @@ class OverkillController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
     var preferencesWindow: PreferencesWindow!
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-    
+    var blackListedProcessNames: [String] = []
+
     override func awakeFromNib() {
         statusItem.title = "Overkill"
         statusItem.menu = statusMenu
+        
+        self.blackListedProcessNames = ["iTunes", "Photos"] // TODO: move to preferences
+        startListening()
     }
-    
 
     @IBAction func didClickPreferences(_ sender: Any) {
         preferencesWindow = PreferencesWindow()
@@ -26,5 +29,28 @@ class OverkillController: NSObject {
 
     @IBAction func didClickExit(_ sender: Any) {
         NSApplication.shared().terminate(self)
+    }
+    
+    func startListening() {
+        let n = NSWorkspace.shared().notificationCenter
+        n.addObserver(self, selector: #selector(self.appWillLaunch(note:)),
+                      name: .NSWorkspaceWillLaunchApplication,
+                      object: nil)
+    }
+    
+    func appWillLaunch(note:Notification) {
+        if let processName:String = note.userInfo?["NSApplicationName"] as? String {
+            if let processId = note.userInfo?["NSApplicationProcessIdentifier"] as? Int {
+                print(processName)
+                if self.blackListedProcessNames.contains(processName) {
+                    print("Killing " + processName)
+                    self.killProcess(processId, processName)
+                }
+            }
+        }
+    }
+    func killProcess(_ processId:Int,_ processName:String) {
+        let process = NSRunningApplication.init(processIdentifier: pid_t(processId))
+        process?.forceTerminate()
     }
 }
